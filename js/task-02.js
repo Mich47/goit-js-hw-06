0c4ebbadd8688a56b403852db332035b0cd7706f
Dmytro Muzyka <mmitya889@gmail.com>
2022-09-29 22:54:39 +0300

    add task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,25 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const list = document.querySelector('#ingredients');
+
+//1 =================================
+
+const listArr = [];
+
+ingredients.forEach(item => {
+  const listItem = document.createElement('li');
+  listItem.textContent = item;
+  listItem.setAttribute('class', 'item');
+  listArr.push(listItem);
+});
+
+list.append(...listArr);
+
+//2 =================================
+
+// ingredients.forEach(item => {
+//   const listItem = document.createElement('li');
+//   listItem.textContent = item;
+//   listItem.setAttribute('class', 'item');
+//   list.append(listItem);
+// });
