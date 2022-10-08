5cc5a616890782df4b128954b7ade5eb1a4a6f2b
Dmytro Muzyka <mmitya889@gmail.com>
2022-10-08 22:46:21 +0300

    add task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorSpanRef = document.querySelector('.color');
+const btnRef = document.querySelector('.change-color');
+
+btnRef.addEventListener('click', onBtn);
+
+function onBtn() {
+  const hexColor = getRandomHexColor();
+  colorSpanRef.textContent = hexColor;
+  document.body.setAttribute('style', `background-color: ${hexColor}`);
+}
