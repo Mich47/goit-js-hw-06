55fb800290b1a7a5ab2b18c450f4ce37747b5b0d
Dmytro Muzyka <mmitya889@gmail.com>
2022-10-08 10:37:35 +0300

    add task-08.js not finish

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formRef = document.querySelector('.login-form');
+console.log('formRef ', formRef);
 
+formRef.addEventListener('submit', onForm);
+
+function onForm(event) {
+  event.preventDefault();
+  const formEl = event.target.elements;
+
+  console.log('formEl.password ', formEl.password.value);
+  console.log('formEl.email ', formEl.email.value);
+
+  if (!formEl.email.value || !formEl.password.value) {
+    alert('Empty field!');
+  }
+  console.dir(formEl);
+}
